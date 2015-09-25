db70ecbdd2f83d411c384d29c44ae884c26045e5
Åke Engelbrektson <eson57@users.noreply.github.com>
2015-09-25 16:10:08 +0200

    Create harbour-simplestopwatch-sv.ts

---
+++ translations/harbour-simplestopwatch-sv.ts
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>harbour-simplestopwatch</name>
+    <message>
+        <source>Stopwatch</source>
+        <translation>Stoppur</translation>
+    </message>
+    <message>
+        <source>Tap anywhere to stop</source>
+        <translation>Tryck var som helst för att stoppa</translation>
+    </message>
+    <message>
+        <source>Tap anywhere to start</source>
+        <translation>Tryck var som helst för att starta</translation>
+    </message>
+    <message>
+        <source>Version </source>
+        <translation>Version </translation>
+    </message>
+</context>
+</TS>
