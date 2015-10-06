379300ebb6c7f3f395b7fb17aa16d97e0d9c5226
Ville Witt <ville.witt@gmail.com>
2015-10-06 14:05:08 +0200

    Danish translation by VilleWitt

---
+++ translations/harbour-simplestopwatch-da.ts
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="da" sourcelanguage="en">
+<context>
+    <name>harbour-simplestopwatch</name>
+    <message>
+        <source>Stopwatch</source>
+        <translation>Stopur</translation>
+    </message>
+    <message>
+        <source>Tap anywhere to stop</source>
+        <translation>Tryk et sted for at stoppe</translation>
+    </message>
+    <message>
+        <source>Tap anywhere to start</source>
+        <translation>Tryk et sted for at starte</translation>
+    </message>
+    </message>
+        <source>Version </source>
+        <translation>Version </translation>
+    </message>
+</context>
+</TS>
